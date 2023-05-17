0ccb8371cecee7ccdfff50a42b6b56f180c61bd8
Kobe <harmon@maliza.io>
2023-05-16 21:43:51 -0400

    proper extensions, and saves as resume

--- app.js
+++ app.js
@@ -12,16 +12,99 @@ const upload = multer({ dest: 'uploads/' });
 const bucketName = 'anza-maliza.appspot.com';
 const storage = new Storage();
 
+// Define the valid output extensions
+const validOutputExtensions = {
+    asciidoc: 'adoc',
+    asciidoctor: 'adoc',
+    beamer: 'pdf',
+    bibtex: 'bib',
+    biblatex: 'bib',
+    chunkedhtml: 'html',
+    commonmark: 'md',
+    commonmark_x: 'md',
+    context: 'ctx',
+    csljson: 'json',
+    docbook: 'xml',
+    docbook4: 'xml',
+    docbook5: 'xml',
+    docx: 'docx',
+    dokuwiki: 'txt',
+    epub: 'epub',
+    epub3: 'epub',
+    epub2: 'epub',
+    fb2: 'fb2',
+    gfm: 'md',
+    haddock: 'html',
+    html: 'html',
+    html5: 'html',
+    html4: 'html',
+    icml: 'icml',
+    ipynb: 'ipynb',
+    jats_archiving: 'xml',
+    jats_articleauthoring: 'xml',
+    jats_publishing: 'xml',
+    jats: 'xml',
+    jira: 'txt',
+    json: 'json',
+    latex: 'tex',
+    man: 'man',
+    markdown: 'md',
+    markdown_mmd: 'md',
+    markdown_phpextra: 'md',
+    markdown_strict: 'md',
+    markua: 'md',
+    mediawiki: 'txt',
+    ms: 'ms',
+    muse: 'muse',
+    native: 'hs',
+    odt: 'odt',
+    opml: 'opml',
+    opendocument: 'odt',
+    org: 'org',
+    pdf: 'pdf',
+    plain: 'txt',
+    pptx: 'pptx',
+    rst: 'rst',
+    rtf: 'rtf',
+    texinfo: 'texinfo',
+    textile: 'txt',
+    slideous: 'html',
+    slidy: 'html',
+    dzslides: 'html',
+    revealjs: 'html',
+    s5: 'html',
+    tei: 'xml',
+    typst: 'typst',
+    xwiki: 'txt',
+    zimwiki: 'txt',
+};
+
 app.post('/', upload.single('file'), async (req, res) => {
+    const userIdentifier = req.body.userIdentifier;
     const inputFormat = req.body.input_format;
     const outputFormat = req.body.output_format;
     const inputFile = req.file.path;
-    const outputFile = path.join('/app/output', `output.${outputFormat}`);
 
-    if (!inputFormat || !outputFormat) {
-        res.status(400).send('Both input_format and output_format must be provided.');
+    if (!inputFormat || !outputFormat || !userIdentifier) {
+        res.status(400).send('Both input_format, output_format, and userIdentifier must be provided.');
         return;
     }
+
+    const userFolder = `${userIdentifier}/`;
+    const folderExists = await storage.bucket(bucketName).file(userFolder).exists();
+    if (!folderExists[0]) {
+        await storage.bucket(bucketName).file(userFolder).save('');
+    }
+
+    const [files] = await storage.bucket(bucketName).getFiles({
+        prefix: userFolder,
+    });
+    const resumeVersion = files.length + 1;
+
+    // Determine the output extension based on the valid extensions
+    const outputExtension = validOutputExtensions[outputFormat] || outputFormat;
+    const outputFileName = `ResumeV${resumeVersion}.${outputExtension}`;
+    const outputFile = path.join('/app/output', outputFileName);
 
     execFile(
         'pandoc',
@@ -31,11 +114,11 @@ app.post('/', upload.single('file'), async (req, res) => {
                 res.status(500).send(`Pandoc conversion failed: ${error.message}`);
             } else {
                 try {
-                    const [file] = await storage.bucket(bucketName).upload(outputFile, {
-                        destination: path.basename(outputFile),
+                    await storage.bucket(bucketName).upload(outputFile, {
+                        destination: path.join(userFolder, outputFileName),
                         public: true,
                     });
-                    const downloadUrl = file.metadata.mediaLink;
+                    const downloadUrl = `https://storage.googleapis.com/${bucketName}/${userFolder}${outputFileName}`;
                     res.send({ download_url: downloadUrl });
                 } catch (uploadError) {
                     console.error(uploadError);
